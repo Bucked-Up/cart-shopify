852bb3f44997644a56eedfeeb0b24b338c1d401d
deigo-lps <diego.lopes0909@gmail.com>
2024-05-23 13:28:58 -0300

    adds uk store

--- src/js/variables.js
+++ src/js/variables.js
@@ -4,6 +4,8 @@ const getAccessToken = (country) => {
       return "3461c5b0c16a619e59715ef4d3b64731";
     case "au":
       return "eea8ddd81a7096b8ce5e04a33234ca39";
+    case "uk":
+      return "d74f7d27c6deb652e0c7257e91024c87";
     default:
       return "3f0fe03b9adb374eee07d99b57da77bd";
   }
@@ -15,6 +17,8 @@ const getFetchUrl = (country) => {
       return "https://cad.buckedup.com/api/2021-07/graphql.json";
     case "au":
       return "https://aud.buckedup.com/api/2021-07/graphql.json";
+    case "uk":
+      return "https://gbp.buckedup.com/api/2021-07/graphql.json";
     default:
       return "https://secure.buckedup.com/api/2021-07/graphql.json";
   }
