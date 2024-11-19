f24b872010b0cefcd2d119917ebd6a511e608795
deigo-lps <diego.lopes0909@gmail.com>
2024-11-19 17:49:38 -0300

    removes intellimize.ready

--- src/js/modules/intellimize.js
+++ src/js/modules/intellimize.js
@@ -1,18 +1,16 @@
 import { getTopLevelDomain } from "./dataLayer.js";
 
 const handleIntellimize = () => {
-  try{
+  try {
     const cookieConfig = `path=/; domain=${getTopLevelDomain()};max-age=3600`;
-    intellimize.ready(()=>{
-      const userId = intellimize.getUserId()
-      const userDomain = "shopifyCheckout"
-      intellimize.setUserId(userDomain, userId);
-      document.cookie = `intellimize_user_id=${userId};${cookieConfig}`;
-      console.log(userDomain,userId)
-    })
-  }catch(e){
-    console.error(e)
+    const userId = intellimize.getUserId();
+    const userDomain = "shopifyCheckout";
+    intellimize.setUserId(userDomain, userId);
+    document.cookie = `intellimize_user_id=${userId};${cookieConfig}`;
+    console.log(userDomain, userId);
+  } catch (e) {
+    console.error(e);
   }
-}
+};
 
 export default handleIntellimize;
