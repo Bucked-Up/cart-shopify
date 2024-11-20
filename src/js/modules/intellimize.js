b7b7e1a466382c72f37e06aded7fe554922661f3
deigo-lps <diego.lopes0909@gmail.com>
2024-11-20 17:09:53 -0300

    adds own user id to intellimize

--- src/js/modules/intellimize.js
+++ src/js/modules/intellimize.js
@@ -1,14 +1,30 @@
 import { getTopLevelDomain } from "./dataLayer.js";
 
+const cookieConfig = `path=/; domain=${getTopLevelDomain()};max-age=3600`;
+
+const getCookie = (name) => {
+  const cookies = document.cookie.split(";");
+  for (let cookie of cookies) {
+    const splited = cookie.split("=");
+    if (splited[0].trim() == name) return splited[1];
+  }
+};
+
+const getUserId = () => {
+  let userId = getCookie("intellimize_user_id");
+  if (!userId) {
+    userId = `${Date.now()}${Math.floor(10000 + Math.random() * 90000)}`;
+    document.cookie = `intellimize_user_id=${userId};${cookieConfig}`;
+  }
+  return userId;
+};
+
 const handleIntellimize = () => {
   try {
-    intellimize.ready(() => {
-      const cookieConfig = `path=/; domain=${getTopLevelDomain()};max-age=3600`;
-      const userId = intellimize.getUserId();
+    intellimize.ready(function () {
+      const userId = getUserId();
       const userDomain = "shopifyCheckout";
       intellimize.setUserId(userDomain, userId);
-      document.cookie = `intellimize_user_id=${userId};${cookieConfig}`;
-      console.log(userDomain, userId);
     });
   } catch (e) {
     console.error(e);
