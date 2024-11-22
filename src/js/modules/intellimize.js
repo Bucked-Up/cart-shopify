986b020006551ba4b88ccbea67ee85f0db50384e
deigo-lps <diego.lopes0909@gmail.com>
2024-11-22 15:54:38 -0300

    wait for rl_anonymous_id

--- src/js/modules/intellimize.js
+++ src/js/modules/intellimize.js
@@ -1,7 +1,3 @@
-// import { getTopLevelDomain } from "./dataLayer.js";
-
-// const cookieConfig = `path=/; domain=${getTopLevelDomain()};max-age=3600`;
-
 const getCookie = (name) => {
   const cookies = document.cookie.split(";");
   for (let cookie of cookies) {
@@ -10,21 +6,25 @@ const getCookie = (name) => {
   }
 };
 
-// const getUserId = () => {
-//   let userId = getCookie("intellimize_user_id");
-//   if (!userId) {
-//     userId = `${Date.now()}${Math.floor(10000 + Math.random() * 90000)}`;
-//     document.cookie = `intellimize_user_id=${userId};${cookieConfig}`;
-//   }
-//   return userId;
-// };
+
+const getUserId = (i = 0) => {
+  const userId = getCookie("rl_anonymous_id");
+  if (userId) return userId;
+  if (i >= 50) {
+    console.warn("no id for intellimize");
+    return;
+  }
+  setTimeout(() => checkUserId(i + 1), 100);
+};
 
 const handleIntellimize = () => {
   try {
     intellimize.ready(function () {
-      const userId = getCookie("rl_anonymous_id");
+      const userId = getUserId()
+      console.log(userId);
       const userDomain = "shopifyCheckout";
       intellimize.setUserId(userDomain, userId);
+      console.log("has set user", userId);
     });
   } catch (e) {
     console.error(e);
